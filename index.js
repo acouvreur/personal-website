5040babe70a083889e1c50f34b88265883cbe5fe
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2019-01-27 00:22:51 +0100

    new homepage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import compression from 'compression'
 import express from 'express'
 import helmet from 'helmet'
 import ejs from 'ejs'
@@ -7,18 +8,11 @@ const app = express();
 const port = process.env.PORT || 1337
 
 app.use(helmet())
-app.use(express.static(__dirname + '/public'));
+app.use(compression());
+app.use(express.static('public'));
 
 const server = app.listen(port, () => {
     console.log(`Express running → PORT ${server.address().port}`)
 })
 
-app.get('/', (req, res) => {
-    res.render(__dirname + '/pages/index.ejs', {title: "Alexis Couvreur"})
-})
-
-// Must be last
-// Handle 404 not found
-app.get('*', (req, res) => {
-    res.render(__dirname + '/pages/404.ejs', {title: "Whoops..."})
-})
+// app.use(serveStatic('pages', {'index': ['default.html']}))
